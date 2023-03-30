e5122ca07df69856519632d704879078b5f919de
ahrniloy <99423385+ahrneloy@users.noreply.github.com>
2023-03-30 06:41:48 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "azaharulrashid.github.io",
+  base: "ahrneloy.github.io",
   plugins: [react()],
 })
