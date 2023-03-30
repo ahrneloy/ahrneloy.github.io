d97b2016151b2ab5cd968e452ea5a5f039012fad
ahrniloy <99423385+ahrneloy@users.noreply.github.com>
2023-03-30 06:53:13 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "ahrneloy.github.io",
-  plugins: [react()],
+   plugins: [react()],
++  base: '/vite-deploy-demo/'
 })
